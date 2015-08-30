375a84796e0dbb3b171d727997a69edc46e9121e
Andrew McPherson <andrewmcp333@gmail.com>
2015-08-30 15:39:32 -0700

    Added a store that feeds into the view.

--- source/index.js
+++ source/index.js
@@ -2,19 +2,47 @@ window.React = require("react")
 window.Phlux = require("phlux")
 window.Three = require("three")
 
+window.Loop = require("<scripts>/utilities/Loop")
+window.Input = require("<scripts>/utilities/Input")
+
 window.WIDTH = 20
 window.HEIGHT = 15
 
 var FrameView = require("<scripts>/views/FrameView")
 var PlanetView = require("<scripts>/views/PlanetView")
 
+var PlanetStore = Phlux.createStore({
+    data: {
+        resolution: 16,
+        rotation: {
+            x: 0.5,
+            y: 0,
+            z: 0
+        },
+        size: 2.25,
+    },
+    update: function(tick) {
+        var planet = this.data
+        planet.rotation.y += 0.25 * tick
+        this.trigger()
+    }
+})
+
 var GameView = React.createClass({
+    mixins: [
+        Phlux.connectStore(PlanetStore, "planet")
+    ],
     render: function() {
         return (
             <FrameView aspect-ratio="4x3">
-                <PlanetView/>
+                <PlanetView data={this.state.planet}/>
             </FrameView>
         )
+    },
+    componentDidMount: function() {
+        Loop(function(tick) {
+            PlanetStore.update(tick)
+        })
     }
 })
 
--- source/scripts/views/PlanetView.js
+++ source/scripts/views/PlanetView.js
@@ -7,50 +7,58 @@ var PlanetView = React.createClass({
     },
     renderStyle: function() {
         return {
-            width: WIDTH + "em",
-            height: HEIGHT + "em"
+            top: "0em",
+            left: "0em",
+            width: "50%",
+            height: "50%",
+            position: "absolute",
         }
     },
     componentDidMount: function() {
-        var scene = new Three.Scene()
-        var camera = new Three.PerspectiveCamera(75, 160 / 120, 0.1, 1000)
-        var renderer = new Three.WebGLRenderer()
-        renderer.setSize(120, 90)
+        var planet = this.props.data
 
-        window.setTimeout(function() {
-            renderer.setSize(1280, 720)
-            renderer.render(scene, camera)
-            element.style.width = WIDTH + "em"
-            element.style.height = HEIGHT + "em"
-        }, 5000)
+        this.vars.scene = new Three.Scene()
+        this.vars.camera = new Three.PerspectiveCamera(75, 1280 / 960, 0.1, 1000)
+        this.vars.renderer = new Three.WebGLRenderer()
 
-        var element = renderer.domElement
-        element.style.width = WIDTH + "em"
-        element.style.height = HEIGHT + "em"
-        this.refs.view.getDOMNode().appendChild(element)
+        this.vars.geometry = new Three.SphereGeometry(1 + planet.size, 32, 32)
+        this.vars.material = new Three.MeshLambertMaterial()
+        this.vars.material.map = Three.ImageUtils.loadTexture("./assets/images/earth.jpg", {}, function() {
+            this.vars.renderer.render(this.vars.scene, this.vars.camera)
+        }.bind(this))
+        this.vars.material.map.minFilter = Three.LinearFilter //?!
+        this.vars.mesh = new Three.Mesh(this.vars.geometry, this.vars.material)
+        this.vars.mesh.rotation.x = planet.rotation.x
+        this.vars.mesh.rotation.y = planet.rotation.x
+        this.vars.scene.add(this.vars.mesh)
 
-        var geometry = new Three.SphereGeometry(3.25, 32, 32)
-        var material = new Three.MeshLambertMaterial(/*{color: 0x0FF00}*/)
-        material.map = Three.ImageUtils.loadTexture("./assets/images/earth.jpg", {}, function() {
-            console.log("loaded")
-            renderer.render(scene, camera)
-        })
-        material.map.minFilter = Three.LinearFilter //?!
-        var planet = new Three.Mesh(geometry, material)
-        planet.rotation.x += 0.45
-        planet.rotation.y += 0.45
-        scene.add(planet)
+        this.vars.camera.position.z = 5
 
-        camera.position.z = 5
+        this.vars.light = new Three.HemisphereLight(0xFFFFFF, 0x000000, 1)
+        //this.vars.light = new Three.DirectionalLight(0xFFFFFF, 1)
+        this.vars.light.position.set(250, 250, 500)
+        this.vars.scene.add(this.vars.light)
 
-        //var light = new Three.HemisphereLight(0xFFFFFF, 0x000000, 0.5)
-        var light = new Three.DirectionalLight(0xFFFFFF, 1)
-        light.position.set(250, 500, 500)
-        scene.add(light)
+        this.vars.renderer.setSize(1280 / planet.resolution, 960 / planet.resolution)
+        this.vars.renderer.render(this.vars.scene, this.vars.camera)
 
-        renderer.render(scene, camera)
-        //console.log(element.toDataURL("image/png"))
-    }
+        this.vars.view = this.vars.renderer.domElement
+        this.vars.view.style.width = "100%"
+        this.vars.view.style.height = "100%"
+        this.refs.view.getDOMNode().appendChild(this.vars.view)
+
+        //console.log(view.toDataURL("image/png"))
+    },
+    componentDidUpdate: function() {
+        var planet = this.props.data
+
+        this.vars.mesh.rotation.x = planet.rotation.x
+        this.vars.mesh.rotation.y = planet.rotation.y
+        this.vars.mesh.rotation.z = planet.rotation.z
+
+        this.vars.renderer.render(this.vars.scene, this.vars.camera)
+    },
+    vars: {}
 })
 
 module.exports = PlanetView
